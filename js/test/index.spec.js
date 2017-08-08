27159676e65ea99846b7afb7801713b373130b7c
Mike Jensen <jjensen.mike@gmail.com>
2017-08-08 14:25:33 -0400

    Updated tests to add getAllSecretsFiltered

--- js/test/index.spec.js
+++ js/test/index.spec.js
@@ -31,6 +31,7 @@ describe('index', () => {
       credstash.listSecrets.should.exist;
       credstash.putSecret.should.exist;
       credstash.getAllSecrets.should.exist;
+      credstash.getAllSecretsFiltered.should.exist;
       credstash.getAllVersions.should.exist;
       credstash.getSecret.should.exist;
       credstash.deleteSecrets.should.exist;
@@ -925,6 +926,96 @@ describe('index', () => {
 
       const credstash = defCredstash();
       return credstash.getAllSecrets()
+        .then((res) => {
+          Object.keys(res).length.should.equal(2);
+          res[item3.name].should.equal(item3.plainText);
+        });
+    });
+  });
+
+  describe('#getAllSecretsFiltered', () => {
+    let items;
+    let kms;
+
+    const item1 = encryption.filteredItem;
+    const item2 = encryption.filteredCredstashKey;
+
+    function addItem(item) {
+      items[item.name] = items[item.name] || {};
+      items[item.name][item.version] = {
+        contents: item.contents,
+        key: item.key,
+        hmac: item.hmac || item.hmacSha256,
+        name: item.name,
+        version: item.version,
+      };
+      kms[item.key] = item.kms;
+    }
+
+    beforeEach(() => {
+      items = {};
+      kms = {};
+
+      addItem(item1);
+      addItem(item2);
+
+      AWS.mock('DynamoDB.DocumentClient', 'scan', (params, cb) => {
+        const Items = [];
+        Object.keys(items).forEach((name) => {
+          const next = items[name];
+          Object.keys(next).forEach(version => Items.push(next[version]));
+        });
+        cb(undefined, { Items });
+      });
+
+      AWS.mock('DynamoDB.DocumentClient', 'get', (params, cb) => {
+        const Item = items[params.Key.name][params.Key.version];
+        cb(undefined, { Item });
+      });
+
+      AWS.mock('KMS', 'decrypt', (params, cb) => {
+        cb(undefined, kms[params.CiphertextBlob.toString('base64')]);
+      });
+    });
+
+    it('should return all secrets', () => {
+      const credstash = defCredstash();
+      return credstash.getAllSecretsFiltered({ context: { project: 'credstash', env: 'test' } })
+        .then((res) => {
+          Object.keys(res).length.should.equal(2);
+          const unsorted = Object.keys(res);
+          const sorted = Object.keys(res).sort();
+          unsorted.should.deep.equal(sorted);
+        });
+    });
+
+    it('should ignore bad secrets', () => {
+      const item3 = Object.assign({}, item1);
+      item3.contents += 'hello broken';
+      item3.name = 'differentName';
+      addItem(item3);
+      const credstash = defCredstash();
+      return credstash.getAllSecretsFiltered({ context: { project: 'credstash', env: 'test' } })
+        .then((res) => {
+          Object.keys(res).length.should.equal(2);
+          const unsorted = Object.keys(res);
+          const sorted = Object.keys(res).sort();
+          unsorted.should.deep.equal(sorted);
+        });
+    });
+
+    it('should return all secrets, but only latest version', () => {
+      const item3 = Object.assign({}, item1);
+      item3.version = item3.version.replace('1', '2');
+      item3.plainText = 'This is a new plaintext';
+      const encrypted = encrypter.encrypt(undefined, item3.plainText, item3.kms);
+      item3.contents = encrypted.contents;
+      item3.hmac = encrypted.hmac;
+
+      addItem(item3);
+
+      const credstash = defCredstash();
+      return credstash.getAllSecretsFiltered({ context: { project: 'credstash', env: 'test' } })
         .then((res) => {
           Object.keys(res).length.should.equal(2);
           res[item3.name].should.equal(item3.plainText);
